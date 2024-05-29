6ab75aa6b7440ecbc1bd67ee521917394f2bf089
drumdidam <drumdidam@gmail.com>
2024-05-29 15:37:03 +0200

    first commit

---
+++ build/CMakeFiles/USB.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/USB.dir/main.cpp.o"
+  "CMakeFiles/USB.dir/main.cpp.o.d"
+  "USB"
+  "USB.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/USB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/USB.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for USB.
+# This may be replaced when dependencies are built.
